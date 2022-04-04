b18a4612da458a57a403f69d475db844f830d5c8
dibamirza <dimirza@cs.ucsb.edu>
2022-04-04 10:20:32 -0700

    lecture 1 and 2 code

---
+++ lect1/lect1.cpp
@@ -0,0 +1,9 @@
+//lect1.cpp
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout<<"Hello!"<<endl;
+    return 0;
+}
---
+++ lect2/linkedlist.cpp
@@ -0,0 +1,61 @@
+//linkedlist.cpp
+#include<iostream>
+#include <vector>
+using namespace std;
+
+struct Node{
+    string data;
+    Node* next; // self-referencing type
+};
+
+// Precondition: @param1 : LinkeList, @param2: string value
+// Postcondition: Insert value to the front of the list and return 
+// the resulting linked list
+//
+Node* insertToFrontOfList(Node* h , string value){
+    //Create a new node
+    // Node* p = new Node {value, h};
+    // Update the head pointer
+    // h = p;
+    // p->next = h;
+    // Return the modified linked list
+    // return p;
+    return new Node {value, h};
+
+}
+void printList(Node* h){
+    Node* t = h; // t  is a traversal 
+    while(t){ // same as while (t!=nullptr)
+        cout<<t->data<<" ";
+        t = t->next;
+    }
+    cout<<endl;
+}
+
+void clearList(Node* h){
+    if(!h) return;
+    clearList(h->next);
+    delete h; // delete nullptr; OKAY!
+}
+
+int main(int argc, char const *argv[])
+{
+    vector<string> TAs {"Tina", "Zack", "Ganesh", "Roman"};
+    //Node n1 {"Apoorva", nullptr}; //n1 is on the stack
+    //Node* p = new Node {"Apoorva", nullptr}; // *p on the heap
+
+    //Create an empty linked list
+    Node* head = nullptr; 
+    head = insertToFrontOfList(head, TAs[0]);
+    printList(head);
+    head = insertToFrontOfList(head, TAs[1]);
+    printList(head);
+    head = insertToFrontOfList(head, TAs[2]);
+    printList(head);
+    clearList(head);
+    head = nullptr;
+    printList(head);
+
+
+    return 0;
+}
